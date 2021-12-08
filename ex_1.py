0d6d3b5dae4c6ab8adeced271411858b15e91682
galerra <artem-newalex@mail.ru>
2021-12-09 01:01:38 +0300

    'Ok'

---
+++ ex_1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8
+if __name__ == "__main__":
+    for l in range(1, 10):
+        for k in range(1, 10):
+            print(l, " * ", j, " = ", l * k)
+        print()
